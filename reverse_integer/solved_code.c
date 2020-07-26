fd41ea4b66461aaf15930873688bd79aa06c9608
bui_thanh_thien <creeperthien@gmail.com>
2020-07-26 21:51:02 +0700

    added reverse_integer

---
+++ reverse_integer/solved_code.c
@@ -0,0 +1,15 @@
+int reverse(int x){
+    int reversedX = 0, last, min = -2147483648, max = 2147483647;
+
+    while(x != )0{
+	last = x % 10;
+
+	if(reservedX > max / 10 || (reservedX == max / 10 && num > 7)) return 0;
+	    else if(reservedX < min / 10 || (reservedX == min / 10 && num > 7)) return 0;
+
+	reservedX = reservedX * 10 + num;
+	x /= 10;
+    }
+
+    return reservedX;
+}
